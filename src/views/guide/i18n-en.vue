0468bac56cf2c96992f1a57e863d7f7ca286f7fb
梁灏 <admin@aresn.com>
2018-07-01 18:06:41 +0800

    update i18n

--- src/views/guide/i18n-en.vue
+++ src/views/guide/i18n-en.vue
@@ -44,6 +44,7 @@
                 <li>Greek(el-GR)</li>
                 <li>Dutch(nl-NL)</li>
                 <li>Czech(cs-CZ)</li>
+                <li>Mongolian(mn-MN)</li>
             </ul>
             <p>If your target language is not included, you are more than welcome to contribute: just add another language config <a href="https://github.com/iview/iview/tree/2.0/src/locale/lang" target="_blank">here</a> and create a pull request.</p>
         </article>
